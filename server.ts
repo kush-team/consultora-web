0cfef679ee330672ca1a1c8585c2c96b97c10a76
Maximiliano Biondo <mbiondo@goblab.org>
2021-01-05 01:08:01 -0300

    Merge

--- server.ts
+++ server.ts
@@ -9,11 +9,22 @@ import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 import { ok } from 'assert';
 
+const nodemailer = require('nodemailer');
+
+const bodyParser = require('body-parser')
+
+
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
   const distFolder = join(process.cwd(), 'dist/maria-web-site/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+
+  //
+  server.use(bodyParser.urlencoded({ extended: false }));
+
+  // parse application/json
+  server.use(bodyParser.json());
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
   server.engine('html', ngExpressEngine({
@@ -26,7 +37,37 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
 
-  server.post('/send-email', (req, res) => { res.json({status: 'ok'}) });
+  server.post('/send-email', async (req, res) => {
+    let testAccount = await nodemailer.createTestAccount();
+    console.log(req.body);
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: testAccount.user, // generated ethereal user
+        pass: testAccount.pass, // generated ethereal password
+      },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Fred Foo 👻" <foo@example.com>', // sender address
+      to: "bar@example.com, baz@example.com", // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    res.json({message: info.messageId});
+  });
   // Serve static files from /browser
 
   server.get('*.*', express.static(distFolder, {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
-    //enabled: true,
-    //content: ['./src/**/*.html', './src/**/*.ts'],
+    enabled: true,
+    content: ['./src/**/*.html', './src/**/*.ts'],
   },
   darkMode: "media", // or 'media' or 'class'
   theme: {
